3647cb7c7ff51aaa6acd598e570f5cd137f24dc6
Bianca Szecowka <bianca.isl@gmail.com>
2023-09-08 21:28:01 -0300

    Bianca8sept2

--- src/js/component/Destacado.jsx
+++ src/js/component/Destacado.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const Destacado = (props) => {
-    return (
-      <div className="contenedor-home">
-        <img src={props.img} alt="Description of the image" className="img-destacado" />
-        <div className="contenido-destacado">
-          <h1>{props.titulo}</h1>
-          <p>{props.texto}</p>
-          <button className="boton-destacado" >{props.label}</button>
-        </div>
+  return (
+    <div className="card h-100">
+      <div className="card-body">
+        <h2 className="card-title">{props.titulo}</h2>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+          magni quas ex numquam, maxime minus quam molestias corporis quod,
+          ea minima accusamus.
+        </p>
       </div>
-    );
-  };
-  export default Destacado;
+      <div className="card-footer">
+        <a className="btn btn-primary btn-sm" href="#!">
+          More Info
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Destacado;
 
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import Destacado from './Destacado';
-import forum from "./../img/forum.jpeg";
+import img1 from "./../img/img1.jpeg";
+import img2 from "./../img/img2.jpeg";
+import img3 from "./../img/img3.png";
 import background from "./../img/background.jpg"
 
 
@@ -10,17 +12,104 @@ import background from "./../img/background.jpg"
 const Home = () => {
 	return (
 		<>
-			<div className="container">
-				<img img src={background} alt="Fondo" className="background-image" />
-				<div className="content">
-					<NavBar diabeticlink={"DiabeticLink"} quienes={"Quiénes Somos"} foro={"Foro"} cambioregalo={"Te lo Cambio/Regalo"} registrologin={"Registro/Login"} />
-					<div className="contenedor-destacado">
-						<Destacado img={forum} titulo={"Foro"} texto={"Ingresa a este espacio para hacer preguntas y conectar con otras personas que también son diabéticas."} label={"Entrar"} />
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<div className="container px-5">
+					<a className="navbar-brand" href="#!">
+						Start Bootstrap
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#!">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#!">
+									About
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#!">
+									Contact
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#!">
+									Services
+								</a>
+							</li>
+						</ul>
 					</div>
-					<Footer footer={"Copyright @ Your Website 2023"} />
+				</div>
+			</nav>
+			{/* Page Content*/}
+			<div className="container px-4 px-lg-5">
+				{/* Heading Row*/}
+				<div className="row gx-4 gx-lg-5 align-items-center my-5">
+					<div className="col-lg-7">
+						<img
+							className="img-fluid rounded mb-4 mb-lg-0"
+							src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg"
+							alt="..."
+						/>
+					</div>
+					<div className="col-lg-5">
+						<h1 className="font-weight-light">Business Name or Tagline</h1>
+						<p>
+							This is a template that is great for small businesses. It doesn't have
+							too much fancy flare to it, but it makes a great use of the standard
+							Bootstrap core components. Feel free to use this template for any
+							project you want!
+						</p>
+						<a className="btn btn-primary" href="#!">
+							Call to Action!
+						</a>
+					</div>
+				</div>
+				{/* Call to Action*/}
+				<div className="card text-white bg-secondary my-5 py-4 text-center">
+					<div className="card-body">
+						<p className="text-white m-0">
+							This call to action card is a great place to showcase some important
+							information or display a clever tagline!
+						</p>
+					</div>
+				</div>
+				{/* Content Row*/}
+				<div className="row gx-4 gx-lg-5">
+					<div className="col-md-4 mb-5">
+						<Destacado titulo={"Foro"} texto={"Haz preguntas, comparte experiencias y sugerencias. Un espacio para crear comunidad."} label={"Entrar"} />
+					</div>
+					<div className="col-md-4 mb-5">
+						<Destacado titulo={"Te lo cambio. Te lo regalo."} texto={"Un espacio para que puedas intercambiar o regalar insumos. Lo que no necesitas, a alguien le puede servir."} label={"Entrar"} />
+					</div>
+					<div className="col-md-4 mb-5">
+						<Destacado titulo={"Novedades"} texto={"Nueva pastelería lowcarb llega con los mejores productos"} label={"Más aquí"} />
+					</div>
 				</div>
 			</div>
+			{/* Footer*/}
+			<footer className="py-5 bg-dark">
+				<div className="container px-4 px-lg-5">
+					<p className="m-0 text-center text-white">
+						Copyright © Your Website 2023
+					</p>
+				</div>
+			</footer>
 		</>
+
 	)
 };
 export default Home;
