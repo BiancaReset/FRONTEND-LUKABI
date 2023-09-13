51da3b044204df8a807af478a074ae237721c03c
luis lopez <luislopezortiz86@gmail.com>
2023-09-12 21:47:58 -0300

    armado del foro

--- src/js/component/ForoTab.jsx
+++ src/js/component/ForoTab.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
 
-export const ForoTab = () => {
+export const ForoTab = ({ imagen, titulo, fecha, user, comentario, cantidad }) => {
     return (
-        <>Soy el ForoTab</>
+        <>
+            <div className="card">
+                <div className="row">
+                    <div className="col-2"><img src={imagen} alt="" /></div>
+                    <div className="col-10">
+                        <div className="row"><strong>{titulo}</strong></div>
+                        <div className="row">{comentario}</div>
+                        <div className="row">publicado por: {user} y fecha: {fecha} </div>
+                        <div className="row">
+                            <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                            </svg> comentarios:{cantidad} </div>
+                            <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
+                                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+                            </svg> compartir</div>
+                            <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+                                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                            </svg> guardar</div>
+                            <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-flag" viewBox="0 0 16 16">
+                                <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21.294 21.294 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21.317 21.317 0 0 0 14 7.655V1.222z" />
+                            </svg> reportar</div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
     )
 }
--- src/js/pages/Foro.jsx
+++ src/js/pages/Foro.jsx
@@ -3,17 +3,26 @@ import { ForoTab } from '../component/ForoTab';
 
 
 const Foro = () => {
+    const publications = [
+        { titulo: "primer post", user: "user 1", fecha: "12/11/2023", comentario: "soy el comentario", cantidad: 158, imagen: "https://picsum.photos/200" },
+        { titulo: "segundo post", user: "user 2", fecha: "12/11/2023", comentario: "soy el comentario", cantidad: 158, imagen: "https://picsum.photos/200" },
+        { titulo: "tercer post", user: "user 1", fecha: "12/11/2023", comentario: "soy el comentario", cantidad: 158, imagen: "https://picsum.photos/200" }
+    ]
     return (
         <>
             <div className="row">
 
                 <div className="col-2">Colibri</div>
-                <div className="col-8"><ForoTab /></div>
+                <div className="col-8"> {publications.map(({ titulo, user, fecha, comentario, cantidad, imagen }, index) => (<ForoTab key={index} titulo={titulo} user={user} fecha={fecha} comentario={comentario} cantidad={cantidad} imagen={imagen} />))} </div>
                 <div className="col-2">4 imagenes</div>
 
 
 
             </div>
+
+
+
+
 
 
 
