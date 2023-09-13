dd9dff59940ccdacdd85db2deb098d38b3e825df
luis lopez <luislopezortiz86@gmail.com>
2023-09-13 19:08:50 -0300

    incio ForoComent

--- src/js/component/ForoTab.jsx
+++ src/js/component/ForoTab.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 export const ForoTab = ({ imagen, titulo, fecha, user, comentario, cantidad }) => {
@@ -8,9 +9,11 @@ export const ForoTab = ({ imagen, titulo, fecha, user, comentario, cantidad }) =
                 <div className="row">
                     <div className="col-2"><img src={imagen} alt="" /></div>
                     <div className="col-10">
-                        <div className="row"><strong>{titulo}</strong></div>
-                        <div className="row">{comentario}</div>
-                        <div className="row">publicado por: {user} y fecha: {fecha} </div>
+                        <Link to="/ForoComent">
+                            <div className="row"><strong>{titulo}</strong></div>
+                            <div className="row">{comentario}</div>
+                            <div className="row">publicado por: {user} y fecha: {fecha} </div>
+                        </Link>
                         <div className="row">
                             <div className="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
                                 <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
