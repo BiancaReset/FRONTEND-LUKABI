8626bf5723228b8c2b82a646276c90e1b39dfdbd
KarinRG <karinromero35@gmail.com>
2023-09-22 19:22:03 -0300

    cambios karin

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,4 @@ import "bootstrap/dist/js/bootstrap.bundle";
 import "../styles/index.css";
 import "../styles/login.css"
 
-<<<<<<< HEAD
-
-
 ReactDOM.render(<App />, document.getElementById('app'))
-=======
-ReactDOM.render(<App />, document.getElementById('app'))
->>>>>>> 957674c0836faae66e8809a948129ed3be12a386
