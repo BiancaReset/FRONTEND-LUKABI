13ce9eb64871354959dbd5ccce9edec7ea01483d
luis lopez <luislopezortiz86@gmail.com>
2023-10-01 23:39:07 -0300

    cambios foro y cambio regalo

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -1,51 +1,92 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Destacado from '../component/Destacado';
-import Imagehome from '../component/Imagehome';
+import React from "react";
+import { Link } from "react-router-dom";
+import Destacado from "../component/Destacado";
+import Imagehome from "../component/Imagehome";
 import colibri from "./../img/colibri.png";
-import Calltoaction from '../component/Calltoaction';
-
-
+import Calltoaction from "../component/Calltoaction";
 
 const Home = () => {
-	return (
-		<>
-			<div className="container px-4 px-lg-5">
-				{/* Heading Row*/}
-				<div className="row gx-4 gx-lg-5 align-items-center my-5">
-					<div className="col-lg-7">
-						<Imagehome image={colibri}
-						/>
-					</div>
-					<div className="col-lg-5">
-						<h2 className="font-weight-light">Testimonio del mes</h2>
-						<p>
-							"El mundo de las redes me ha enseñado conceptos que desconocía sobre mi diabetes". - Aida Gámez.
-							Leer entrevista <a href="https://www.makingdiabeteseasier.com/es/consejos-sobre-diabetes/vivir-con-diabetes/conceptos-sobre-diabetes">aquí</a>
-						</p>
-						<a className="btn btn-outline-primary" href="#!">
-							Leer más
-						</a>
-					</div>
-				</div>
-				<Calltoaction texto={"Tenemos Diabetes. Nos acompaña, no nos detiene. - Chris Olsen."} />
-				{/* Content Row*/}
-				<div className="row gx-4 gx-lg-5">
-					<div className="col-md-4 mb-5">
-						<Destacado titulo={"Foro"} texto={"Haz preguntas, comparte experiencias y sugerencias. Un espacio para crear comunidad."} label={<Link to="/foro">Entrar</Link>} />
-					</div>
-					<div className="col-md-4 mb-5">
-						<Destacado titulo={"Te lo cambio. Te lo regalo."} texto={"Un espacio para que puedas intercambiar o regalar insumos. Lo que no necesitas, a alguien le puede servir."} label={<Link to="/cambioregalo">Entrar</Link>} />
-					</div>
-					<div className="col-md-4 mb-5">
-						<Destacado titulo={"Nuestros Partners"} texto={"Ingresa aquí para conocer a nuestros partners. Empresas que comercializan productos que seguro te van a interesar."} label={<Link to="/comercios">Entrar</Link>} />
-					</div>
-				</div>
-			</div>
-
-		</>
-
-	)
+  return (
+    <>
+      <div className="container px-4 px-lg-5">
+        {/* Heading Row*/}
+        <div className="row gx-4 gx-lg-5 align-items-center my-5">
+          <div className="col-lg-7">
+            <Imagehome image={colibri} />
+          </div>
+          <div className="col-lg-5">
+            <h2 className="font-weight-light">Testimonio del mes</h2>
+            <p>
+              "El mundo de las redes me ha enseñado conceptos que desconocía
+              sobre mi diabetes". - Aida Gámez.
+            </p>
+            <a
+              target="_blank"
+              className="btn btn-outline-primary"
+              href="https://www.makingdiabeteseasier.com/es/consejos-sobre-diabetes/vivir-con-diabetes/conceptos-sobre-diabetes"
+            >
+              Leer más
+            </a>
+          </div>
+        </div>
+        <Calltoaction
+          texto={
+            "Tenemos Diabetes. Nos acompaña, no nos detiene. - Chris Olsen."
+          }
+        />
+        {/* Content Row*/}
+        <div className="row gx-4 gx-lg-5">
+          <div className="col-md-4 mb-5">
+            <Destacado
+              titulo={"Foro"}
+              texto={
+                "Haz preguntas, comparte experiencias y sugerencias. Un espacio para crear comunidad."
+              }
+              label={
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/foro"
+                >
+                  Entrar
+                </Link>
+              }
+            />
+          </div>
+          <div className="col-md-4 mb-5">
+            <Destacado
+              titulo={"Te lo cambio. Te lo regalo."}
+              texto={
+                "Un espacio para que puedas intercambiar o regalar insumos. Lo que no necesitas, a alguien le puede servir."
+              }
+              label={
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/cambioregalo"
+                >
+                  Entrar
+                </Link>
+              }
+            />
+          </div>
+          <div className="col-md-4 mb-5">
+            <Destacado
+              titulo={"Nuestros Partners"}
+              texto={
+                "Ingresa aquí para conocer a nuestros partners. Empresas que comercializan productos que seguro te van a interesar."
+              }
+              label={
+                <Link
+                  style={{ textDecoration: "none", color: "white" }}
+                  to="/comercios"
+                >
+                  Entrar
+                </Link>
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Home;
-
--- src/js/pages/Login.jsx
+++ src/js/pages/Login.jsx
@@ -1,90 +1,85 @@
-import React, { useContext, useState } from 'react';
-import logo from '../img/logo.jpeg';
+import React, { useContext, useState } from "react";
+import logo from "../img/logo.jpeg";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../../store/AppContext";
 
 const Login = () => {
-    const [values, setValues] = useState({
-        username: '',
-        password: '',
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = values;
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setValues({
+      ...values,
+      [name]: value,
     });
+  };
 
-    const { username, password } = values
+  const { actions } = useContext(Context);
+  const navigate = useNavigate();
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setValues({
-            ...values,
-            [name]: value,
-        });
-    };
+  const handleLogin = (event) => {
+    actions.loginRequest(username, password, navigate);
+    event.preventDefault();
+  };
 
-    const { actions } = useContext(Context)
-    const navigate = useNavigate()
-
-
-    const handleLogin = (event) => {
-        actions.loginRequest(username, password, navigate)
-        event.preventDefault();
-
-    };
-
-    return (
-        <div className="container">
-            <div className="text-center mt-5">
-                <main className="form-signin w-100 m-auto">
-                    <form onSubmit={handleLogin}>
-                        <img
-                            className="mb-4"
-                            src={logo}
-                            alt=""
-                            width={100}
-                            height={90}
-                        />
-                        <h1 className="h3 mb-3 fw-normal">Iniciar sesión</h1>
-                        <div className="form-floating">
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="username"
-                                placeholder="Correo electrónico"
-                                required=""
-                                onChange={handleInputChange}
-                                value={values.username}
-                            />
-                            <label htmlFor="floatingInput">Correo electrónico</label>
-                            <div className="invalid-feedback">
-                                Correo electrónico requerido
-                            </div>
-                        </div>
-                        <div className="form-floating">
-                            <input
-                                type="password"
-                                className="form-control"
-                                name="password"
-                                placeholder="Password"
-                                required=""
-                                onChange={handleInputChange}
-                                value={values.password}
-                            />
-                            <label htmlFor="floatingPassword">Password</label>
-                            <div className="invalid-feedback">
-                                Password requerido
-                            </div>
-                        </div>
-                        <div className="checkbox mb-3">
-                            <label>
-                                <input type="checkbox" defaultValue="remember-me" /> Recordarme
-                            </label>
-                        </div>
-                        <button className="w-100 btn btn-lg btn-primary" type="submit">
-                            Ingresar
-                        </button>
-                    </form>
-                </main>
+  return (
+    <div className="container">
+      <div className="text-center mt-5">
+        <main className="form-signin w-100 m-auto">
+          <form onSubmit={handleLogin}>
+            <img className="mb-4" src={logo} alt="" width={100} height={90} />
+            <h1 className="h3 mb-3 fw-normal">Iniciar sesión</h1>
+            <div className="form-floating">
+              <input
+                type="text"
+                style={{ width: "100%" }}
+                className="form-control "
+                name="username"
+                placeholder="Correo electrónico"
+                required=""
+                onChange={handleInputChange}
+                value={values.username}
+              />
+              <label htmlFor="floatingInput">Correo electrónico</label>
+              <div className="invalid-feedback">
+                Correo electrónico requerido
+              </div>
             </div>
-        </div>
-    );
+            <div className="form-floating">
+              <input
+                style={{ width: "100%" }}
+                type="password"
+                className="form-control rounded"
+                name="password"
+                placeholder="Password"
+                required=""
+                onChange={handleInputChange}
+                value={values.password}
+              />
+              <label htmlFor="floatingPassword">Password</label>
+              <div className="invalid-feedback">Password requerido</div>
+            </div>
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" defaultValue="remember-me" /> Recordarme
+              </label>
+            </div>
+            <button
+              className="w-100 btn btn-lg btn-primary mx-auto"
+              type="submit"
+            >
+              Ingresar
+            </button>
+          </form>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
